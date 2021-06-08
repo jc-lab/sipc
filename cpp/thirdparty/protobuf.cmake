a0f4a07f70e8d7b790ba4b51cd312027336f9e23
jichan <joseph@jc-lab.net>
2021-06-09 00:34:29 +0900

    fix: cmake

--- cpp/thirdparty/protobuf.cmake
+++ cpp/thirdparty/protobuf.cmake
@@ -6,26 +6,19 @@ ExternalProject_Add(
         GIT_REPOSITORY https://github.com/protocolbuffers/protobuf.git
         GIT_TAG 70db61a91bae270dca5db2f9837deea11118b148 # v3.17.2
 
-        SOURCE_SUBDIR "cmake"
+        SOURCE_SUBDIR cmake
 
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ${CMAKE_COMMAND} -E echo "Starting $<CONFIG> build"
-        COMMAND ${CMAKE_COMMAND}
-        <SOURCE_DIR>
-        -DBUILD_TYPE=$<CONFIG>
-        -DCMAKE_BUILD_TYPE=$<CONFIG>
-        -DCMAKE_INSTALL_CONFIG_NAME=$<CONFIG>
-        ${SUBPROJECTS_COMMON_CMAKE_ARGS}
-        "-Dprotobuf_BUILD_SHARED_LIBS=OFF"
-        "-Dprotobuf_WITH_ZLIB=OFF"
-        "-Dprotobuf_BUILD_TESTS=OFF"
-        "-Dprotobuf_MSVC_STATIC_RUNTIME=${JCU_SIPC_MSVC_STATIC_RUNTIME}"
-        WORKING_DIRECTORY <BUILD_DIR>
-        COMMAND       ${CMAKE_COMMAND} --build <BINARY_DIR> --config $<CONFIG>
-        COMMAND       ${CMAKE_COMMAND} -E echo "$<CONFIG> build complete"
+        COMMAND ${CMAKE_COMMAND} <SOURCE_DIR>/<SOURCE_SUBDIR> -DBUILD_TYPE=$<CONFIG> -DCMAKE_BUILD_TYPE=$<CONFIG> -DCMAKE_INSTALL_CONFIG_NAME=$<CONFIG> ${SUBPROJECTS_COMMON_CMAKE_ARGS}
+        -Dprotobuf_BUILD_SHARED_LIBS=OFF
+        -Dprotobuf_WITH_ZLIB=OFF
+        -Dprotobuf_BUILD_TESTS=OFF
+        -Dprotobuf_MSVC_STATIC_RUNTIME=${JCU_SIPC_MSVC_STATIC_RUNTIME}
+
+        COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config $<CONFIG>
 
         UPDATE_COMMAND ""
         PATCH_COMMAND ""
         TEST_COMMAND ""
 )
-
