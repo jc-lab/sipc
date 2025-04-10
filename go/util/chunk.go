f7b4f05963becc99cbed218197536693f1e4e3e2
Joseph Lee <joseph@jc-lab.net>
2025-04-10 12:24:46 +0900

    fix: add missed chunk.go

--- go/util/chunk.go
+++ go/util/chunk.go
@@ -1,1 +1,16 @@
 package util
+
+func Chunk(p []byte, maxSize int, handler func(chunk []byte) error) error {
+	for len(p) > 0 {
+		chunkSize := len(p)
+		if chunkSize > maxSize {
+			chunkSize = maxSize
+		}
+		chunkBuf := p[:chunkSize]
+		p = p[chunkSize:]
+		if err := handler(chunkBuf); err != nil {
+			return err
+		}
+	}
+	return nil
+}
